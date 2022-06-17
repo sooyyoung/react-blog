8699f65f3fa776a09478b0e4b85e57949cc87fbf
sooyyoung <sooyyyoung@gmail.com>
2022-06-17 19:29:44 +0900

    컴포넌트 생성 및 파일 분리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Homepage from './pages/homepage/Homepage'
+import Post from './pages/post/Post'
+import './App.css'
+
 function App() {
-	return <></>;
+	return (
+		<>
+			<Homepage />
+			<Post />
+		</>
+	)
 }
 
 export default App;
---
+++ src/pages/post/Post.jsx
@@ -0,0 +1,10 @@
+import Header from '../../components/header/Header'
+import './post.css'
+
+export default function Post(){
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
