b637913ef763446c32e6813ce2752f4105e14632
sooyyoung <sooyyyoung@gmail.com>
2022-06-18 23:19:17 +0900

    Banner 컴포넌트 추가

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,10 +1,12 @@
 import Header from '../../components/header/Header'
+import Banner from '../../components/banner/Banner'
 import './homepage.css'
 
 export default function Homepage(){
     return (
         <div>
             <Header />
+            <Banner />
         </div>
     )
 }
