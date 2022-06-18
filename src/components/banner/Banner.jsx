116c75914e6b20b1c4dbe8e2047239cf0d56cac3
sooyyoung <sooyyyoung@gmail.com>
2022-06-18 23:18:50 +0900

    Banner 컴포넌트 완성

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,1 +1,26 @@
 import './banner.css'
+
+export default function Banner(){
+    return (
+        <div className='banner'>
+            <div className='max-width'>
+				<div className="banner-contents">
+                    <p class="sub-text">React Blog</p>
+                    <p class="main-text">My BLOG</p>
+                    <p class="description">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nostrum dolor provident beatae deserunt magnam, illo neque. Maiores ipsum commodi dolorum beatae aliquid laboriosam est
+					</p>
+				</div>
+			</div>
+        </div>
+    )
+}
+
+
+<div class="banner" style="background-image: url(./images/post-background6.jpg)">
+			<div class="max-width">
+				<div class="banner-contents">
+					<p class="today">May <em>25</em> Wednesday</p>
+				</div>
+			</div>
+		</div>
