aecbf2ee994043b37b53bdce2818013efde1fbe8
sooyyoung <sooyyyoung@gmail.com>
2022-06-20 19:43:06 +0900

    Footer 컴포넌트 완성

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,1 +1,14 @@
 import './footer.css'
+
+export default function Footer(){
+    return (
+        <>
+            <footer>
+                <div className="max-width">
+                    <h2>©Weniv Corp.</h2>
+                    <button className="top-button">TOP</button>
+                </div>
+		    </footer>
+        </>
+    )
+}
