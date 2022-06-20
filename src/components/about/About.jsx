ae6f50d3d5760f370e8b897e9cde4f4e4d74056f
sooyyoung <sooyyyoung@gmail.com>
2022-06-20 17:22:26 +0900

    About 컴포넌트 완성

---
+++ src/components/about/About.jsx
@@ -0,0 +1,50 @@
+import './about.css'
+
+import Facebook from '../../assets/Facebook.svg';
+import Twitter from '../../assets/Twitter.svg';
+import Instagram from '../../assets/Instagram.svg';
+import Github from '../../assets/Github.svg';
+
+export default function About(){
+    return (
+        <div className="about">
+            <h2>About Me</h2>
+            <img src="../../../assets/profile.jpg" alt="" className="user-profile" />
+            <p className="user-name">Chilli</p>
+            <p className="user-description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <h3>Categories</h3>
+            <ul className="categories">
+                <li><a href="#">Life</a></li>
+                <li><a href="#">Style</a></li>
+                <li><a href="#">Tech</a></li>
+                <li><a href="#">Sport</a></li>
+                <li><a href="#">Photo</a></li>
+                <li><a href="#">Develop</a></li>
+                <li><a href="#">Music</a></li>
+            </ul>
+            <h3>Follow</h3>
+            <ul className="sns">
+                <li>
+                    <a href="#">
+                        <img src={Facebook} alt="Facebook" />
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src={Twitter} alt="Twitter" />
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src={Instagram} alt="Instagram" />
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src={Github} alt="GitHub" />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
