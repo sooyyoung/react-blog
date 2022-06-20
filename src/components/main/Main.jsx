5eca28908919807af1ff14d4bf9a26ac6ee43656
sooyyoung <sooyyyoung@gmail.com>
2022-06-20 19:53:28 +0900

    About, PostList 컴포넌트 추가

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,1 +1,16 @@
+import About from '../../components/about/About'
+import PostList from '../postList/PostList'
 import './main.css'
+
+export default function Main(){
+    return (
+        <>
+            <main>
+                <div className='max-width'>
+                    <PostList />
+                    <About />
+                </div>
+            </main>
+        </>
+    )
+}
