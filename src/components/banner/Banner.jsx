f9245ffd6f25b341ede3cac453db94b5b46804a1
sooyyoung <sooyyyoung@gmail.com>
2022-06-20 13:46:42 +0900

    p 태그 속성 변경

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -5,9 +5,9 @@ export default function Banner(){
         <div className='banner'>
             <div className='max-width'>
 				<div className="banner-contents">
-                    <p class="sub-text">React Blog</p>
-                    <p class="main-text">My BLOG</p>
-                    <p class="description">
+					<p className="sub-text">React Blog</p>
+					<p className="main-text">My BLOG</p>
+					<p className="description">
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nostrum dolor provident beatae deserunt magnam, illo neque. Maiores ipsum commodi dolorum beatae aliquid laboriosam est
 					</p>
 				</div>
